b32095c0a6b2ab30bb07c147eb38db82bb1921cc
Vigor-Lundkvist <vigor.lundkvist@live.se>
2021-09-22 13:11:03 +0200

    Partly done with task 1.

--- Car Dealer v2.0/Car.cs
+++ Car Dealer v2.0/Car.cs
@@ -8,6 +8,18 @@ namespace Car_Dealer_v2._0
 {
     public class Car
     {
+        public int Id { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public string Color { get; set; }
+        public int Km { get; set; }
+        public int Price { get; set; }
+        public int Year { get; set; }
 
+
+        public override string ToString()
+        {
+            return $"{Make} {Model}";
+        }
     }
 }
